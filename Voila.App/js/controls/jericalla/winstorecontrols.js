37c27b5b4e1405e8bd92f986dcaf455abee6972a
pollirrata <jorgegaona@yahoo.com>
2013-10-10 21:56:48 -0500

    10/10

---
+++ Voila.App/js/controls/jericalla/winstorecontrols.js
@@ -0,0 +1,66 @@
+﻿(function () {
+    WinJS.Namespace.define("Jericalla.UI", {
+        PagedList: WinJS.Class.define(function (element, options) {
+            options = options || {};
+            this._setElement(element);
+            this._setItems(options.itemList);
+            this._element.winControl = this;
+            WinJS.UI.setOptions(this, options);
+            this._create();
+        },
+        {
+            //Private
+            _element: null,
+            _itemList: null,
+            _ordered: false,
+            _setElement: function (element) {
+                this._element = element;
+            },
+            _setOrdered: function (ordered) {
+                this._ordered = ordered;
+            },
+            _setItems: function (itemList) {
+                itemList = itemList || [];
+                this._itemList = itemList;
+            },
+            _create: function () {
+                var i = 0,
+                len = this._itemList.length,
+                bulletList = document.createElement('ol');
+                bulletList.id = 'bulletList_' + this._element.id;
+                for (; i < len; i += 1) {
+                    var listItem = document.createElement('li');
+                    listItem.innerText = this._itemList[i];
+                    bulletList.appendChild(listItem);
+                }
+                this._element.appendChild(bulletList);
+            },
+
+            //Public
+            element: {
+                get: function () {
+                    return this._element;
+                }
+            }
+        },
+        { // static members 
+        })
+    });
+
+    //WinJS.Namespace.define("jericalla.UI", {
+    //    AutoComplete: WinJS.Class.define(function (element, options) {
+    //        if (!element || element.tagName.toLowerCase() !== "input")
+    //            throw "input type must be provided";
+    //        options = options || {};
+    //        this._setElement(element);
+    //        this._setitemList(options.itemList);
+    //        this._element.winControl = this;
+    //        WinJS.UI.setOptions(this, options);
+    //        this._createDataList();
+    //    },
+    //    { // instance members 
+    //    },
+    //    { // static members 
+    //    })
+    //});
+});
--- Voila.App/pages/search/detail.js
+++ Voila.App/pages/search/detail.js
@@ -57,22 +57,132 @@
         }
     }
 
+
+
+
+
+
+    $("#goSearch").click(goSearch);
+    $("#goFavorites").click(addFavorites);
+    $("#goPopular").click(goPopular);
+    $("#goInfo").click(goInfo);
+
+
+
+    WinJS.Namespace.define("Jericalla.UI", {
+        PagedList: WinJS.Class.define(function (element, options) {
+            options = options || {};
+            this._setElement(element);
+            this._setItems(options.itemList);
+            this._setListType(options.listType);
+            this._setPageSize(options.pageSize);
+            this._element.winControl = this;
+            WinJS.UI.setOptions(this, options);
+            this._create();
+        },
+        {
+            //Private
+            _element: null,
+            _itemList: null,
+            _ordered: false,
+            _setElement: function (element) {
+                this._element = element;
+            },
+            _setListType: function (listType) {
+                this._listType = listType;
+            },
+            _setPageSize: function (pageSize) {
+                this._pageSize = pageSize;
+            },
+            _setItems: function (itemList) {
+                itemList = itemList || [];
+                this._itemList = itemList;
+            },
+            _create: function () {
+                var i = 0,
+                len = this._itemList.length;
+                //bulletList = document.createElement(this._listType);
+                //bulletList.id = 'bulletList_' + this._element.id;
+                //this._element.setAttribute("list", bulletList.id);
+
+                var bulletList = {};
+
+                if (this._pageSize >= len) {
+
+                    bulletList = document.createElement(this._listType);
+                    bulletList.id = 'bulletList_' + this._element.id;
+
+                    for (; i < len; i += 1) {
+                        var listItem = document.createElement('li');
+                        listItem.innerText = this._itemList[i];
+                        bulletList.appendChild(listItem);
+                    }
+                    this._element.appendChild(bulletList);
+                } else {
+                    var pages = len / this._pageSize;
+                    if (len % this._pageSize > 0) {
+                        pages = Math.floor(len / this._pageSize) + 1;
+                    }
+
+                    for (var p = 2; p <= pages; p++) {
+                        var tab = document.createElement("div");
+                        tab.setAttribute("class", "tab");
+                        tab.setAttribute("data-val", p);
+                        tab.innerText = p;
+                        this._element.appendChild(tab);
+                    }
+
+                    var ulClass = "";
+                    for (; i < pages; i += 1) {
+                        bulletList = document.createElement(this._listType);
+                        bulletList.id = 'bulletList_' + this._element.id + "_" + i;
+
+                        for (var j = i * this._pageSize; j < this._pageSize * (i + 1) && j < len; j += 1) {
+                            var listItem = document.createElement('li');
+                            listItem.innerText = this._itemList[j];
+                            bulletList.appendChild(listItem);
+                        }
+                        bulletList.setAttribute("class", ulClass +  " t"  + (i + 1).toString());
+                        this._element.appendChild(bulletList);
+                        ulClass = "invisible";
+                    }
+                }
+            },
+
+            //Public
+            element: {
+                get: function () {
+                    return this._element;
+                }
+            }
+        },
+        { // static members 
+        })
+    });
+
+
+
     WinJS.UI.Pages.define("/pages/search/detail.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
 
         ready: function (element, options) {
-            recipe = WinJS.Navigation.state;
+
+            var recipe = WinJS.Navigation.state;
 
             var recipeDiv = document.getElementById("recipeDetail");
 
             WinJS.Binding.processAll(recipeDiv, recipe);
 
-            $("#goSearch").click(goSearch);
-            $("#goFavorites").click(addFavorites);
-            $("#goPopular").click(goPopular);
-            $("#goInfo").click(goInfo);
+            //$("ul").css("background-color", "red");
+            $("#recipeDetail .ingredients div").click(function () {
+                $("#recipeDetail .ingredients div").removeClass("selected");
+                $(this).addClass("selected");
 
+                $("#recipeDetail .ingredients ul").addClass("invisible");
+                var id = $(this).attr("data-val");
+                $("#recipeDetail .ingredients ul.t" + id).removeClass("invisible");
+            })
         },
 
         unload: function () {
